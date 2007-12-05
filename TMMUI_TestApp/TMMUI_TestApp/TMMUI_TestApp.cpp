04daadf84d8b586a3cf6ccd5dbcd90faa1dc77b3
rob.g.evans <rob.g.evans@0e52ec8f-463b-0410-b89e-d778e79482b9>
2007-12-05 22:35:16 +0000

    Committing to debug new event system

--- TMMUI_TestApp/TMMUI_TestApp/TMMUI_TestApp.cpp
+++ TMMUI_TestApp/TMMUI_TestApp/TMMUI_TestApp.cpp
@@ -1,33 +1,164 @@
 #include "TMMUI_TestApp.h"
-
+#include <iostream>
+#include <string>
 
 // TenUI Application Entry Point
 IMPLEMENT_TENUI_APP(TMMUI_TestApp)
 
+std::string ltoa(long l){
+	std::string s = "";
+	char c;
+	while(l / 10 > 0 || l % 10 != 0)
+	{
+		c = (char)(l % 10 + 48);
+		s = c + s;
+		l = l / 10;
+	}
+	
+	return s;
+}
+
 namespace TenUI {
 		
 	void TMMUI_TestApp::initRectangleTest(){			
-		Rectangle *tmpRect2;
-		InteractiveRectangle *tmpRect;
-		for(unsigned int i = 0; i < 1280; i+=64){
-			for(unsigned int j = 0; j < 1024; j+=64){
+		/*Rectangle *tmpRect2;
+		DraggableRectangle *tmpRect;
+		Text *tmpText;
+		
+		float w = getScreenWidth(), h = getScreenHeight();
+		
+		for(unsigned int i = 0; i < w; i+=64){
+			for(unsigned int j = 0; j < h; j+=64){
+				tmpText = new Text(NULL,i,j);
 				tmpRect2 = new Rectangle(NULL, i, j, 64, 64, 			// x,y,w,h
-										(i)/1280.0, (j)/1024.0, ((i+j))/1302.0, 	// fill color
-										((i+j))/1302.0, (i)/1280.0, (j)/1024.0, 	// stroke color
-										(j/(1024.0-64))*10.0);	
-				tmpRect = new InteractiveRectangle(NULL, i, j, 64, 64, 			// x,y,w,h
-											  (i)/1280.0, (j)/1024.0, ((i+j))/1302.0, 	// fill color
-											  ((i+j))/1302.0, (i)/1280.0, (j)/1024.0, 	// stroke color
-											  (j/(1024.0-64))*10.0, 0,1,1, tmpRect2);	
-				//TenMilManUI::instance()->addDisplayObject(tmpRect);
-				TMMUI_TestApp::sltMgr->registerObj((UIComponent*)tmpRect);
+										(i+128)/w, (j+128)/h, ((i+j+256))/(w+h), 	// fill color
+										((i+j))/(w+h), (i)/w, (j)/h, 	// stroke color
+										4.0);	
+				tmpRect = new DraggableRectangle(NULL, i, j, 64, 64, 			// x,y,w,h
+						(i)/w, (j)/h, ((i+j))/(w+h), 	// fill color
+						((i+j))/(w+h), (i)/w, (j)/h, 	// stroke color
+						4.0,0,0,1,tmpRect2,tmpText);
+				
+				
+				tmpText->setFont("Calibri");
+				tmpText->setFontSize(12);
+				tmpText->setText(ltoa(tmpRect->getObjectID()));
+				tmpText->setColor("#000");
+				
+				getTenUI()->addDisplayObject(tmpText);
 				getTenUI()->addDisplayObject(tmpRect2);
 				getTenUI()->addDisplayObject(tmpRect);
 			}
-		}
+		}*/
 	}
 	
-	void TMMUI_TestApp::initInteractiveRectangleTest(){			
+	void TMMUI_TestApp::initLotsOfRectanglesTest(){
+		/*Rectangle *tmpRect2;
+		SelectableRectangle *tmpRect = NULL;
+		DraggableRectangle *tmpDrgRect = NULL;
+		Text *tmpText;
+		
+		float w = getScreenWidth(), h = getScreenHeight(), r = 0,g = 0,b = 0;
+		bool condition;
+		for(unsigned int i = 0; i < w; i+=64){
+			for(unsigned int j = 0; j < h; j+=64){
+				
+				condition = (((i/64)%2) != ((j/64)%2));
+				
+				r = (condition ? 1 : 0); //r = (i < (w/2) ? 1 : 0);
+				g = .25;
+				b = (condition ? 0 : 1); //b = (i < (w/2) ? 0 : 1);
+				tmpRect2 = new Rectangle(NULL, i, j, 64, 64, 			// x,y,w,h
+										r, g, b, 	// fill color
+										((i+j))/(w+h), (i)/w, (j)/h, 	// stroke color
+										4.0);
+				tmpText = new Text(NULL,i,j);
+				
+				if(condition)
+				{
+					tmpRect = new SelectableRectangle(NULL, i, j, 64, 64, 			// x,y,w,h
+							r, g, b, 	// fill color
+							((i+j))/(w+h), (i)/w, (j)/h, 	// stroke color
+							4.0,0,0,1,tmpRect2, tmpText);
+				}
+				else
+				{
+					tmpDrgRect = new DraggableRectangle(NULL, i, j, 64, 64, 			// x,y,w,h
+												r, g, b, 	// fill color
+												((i+j))/(w+h), (i)/w, (j)/h, 	// stroke color
+												4.0,0,0,1,tmpRect2, tmpText);
+				}
+				
+				
+				tmpText->setFont("Calibri");
+				tmpText->setFontSize(12);
+				
+				if(condition)
+				tmpText->setText(ltoa(tmpRect->getObjectID()));
+				else
+				tmpText->setText(ltoa(tmpDrgRect->getObjectID()));
+				
+				tmpText->setColor("#000");
+				
+				getTenUI()->addDisplayObject(tmpText);
+				getTenUI()->addDisplayObject(tmpRect2);
+				if(condition)
+				getTenUI()->addDisplayObject(tmpRect);
+				else
+				getTenUI()->addDisplayObject(tmpDrgRect);
+			}
+		}*/
+	}
+	
+	void TMMUI_TestApp::initChildrenTest(){
+		Rectangle *rect = new Rectangle(NULL, 100, 100, 128, 128, .2, .2, .2, 1, 1, 1, 4.0);
+		Text *caption = new Text(NULL,100,100);
+		
+		Rectangle *rect2 = new Rectangle(NULL, 106, 106, 64, 64, .6, .6, .6, 1, 1, 1, 4.0);
+		Text *caption2 = new Text(NULL,106,106);
+		
+		caption->setFont("Calibri");
+		caption->setFontSize(12);
+		caption->setColor("#FFF");
+		caption->setText("");
+		//SelectableRectangle *selRect = new SelectableRectangle(NULL, 100, 100, 128, 128, .2, .2, .2, 1, 1, 1, 4.0,0,0,1,rect, caption);
+		
+		getTenUI()->addDisplayObject(rect);
+		getTenUI()->addDisplayObject(caption);
+		
+		
+		
+		
+		getTenUI()->addDisplayObject(rect2);
+		getTenUI()->addDisplayObject(caption2);
+		
+		caption2->setFont("Calibri");
+		caption2->setFontSize(12);
+		caption2->setColor("#FFF");
+		caption2->setText("child");
+		//DraggableRectangle *drgRect = new DraggableRectangle(NULL, 106, 106, 64, 64, .2, .2, .2, 1, 1, 1, 4.0,0,0,1,rect2, caption2);
+		
+		//getTenUI()->addDisplayObject(selRect);
+		//getTenUI()->addDisplayObject(drgRect);
+		
+		//TableInput_NewArch *tblNA = dynamic_cast<TableInput_NewArch*>(InputManager::getInput(1));
+		/*if(tblNA != 0)
+		{
+			tblNA->horzBar = new Line(NULL, 0, 10, 5, 5,	//x,y,x2,y2
+					1.0,0.0,0.0, 	// stroke color
+					2.0);
+			
+			tblNA->vertBar = new Line(NULL, 5, 0, 5, 10,	//x,y,x2,y2
+							1.0,0.0,0.0, 	// stroke color
+							2.0);
+			
+			getTenUI()->addDisplayObject(tblNA->horzBar);
+			getTenUI()->addDisplayObject(tblNA->vertBar);
+		}*/
+		//selRect->addChild(drgRect);
+	}
+	
+	/*void TMMUI_TestApp::initInteractiveRectangleTest(){			
 			InteractiveRectangle *tmpRect;
 			Rectangle *tmpRect2;
 			int i = 0, j = 100;
@@ -41,54 +172,31 @@ namespace TenUI {
 											   sr, sg, sb, 	// stroke color
 											   sw,0.0,1.0,1.0, tmpRect2);
 			
-			//TMMUI_TestApp::sltMgr->registerObj((UIComponent*)tmpRect2);
-						getTenUI()->addDisplayObject(tmpRect2);
-						
-			TMMUI_TestApp::sltMgr->registerObj((UIComponent*)tmpRect);
+			getTenUI()->addDisplayObject(tmpRect2);
 			getTenUI()->addDisplayObject(tmpRect);
 						
-		}
+		}*/
 	
-	void TMMUI_TestApp::initLineTest(){		
+	/*void TMMUI_TestApp::initLineTest(){		
 		Line *tmpLine;
 		for(unsigned int i = 0; i < 1280; i+=64){
 			for(unsigned int j = 0; j < 1024; j+=64){			
 				tmpLine = new Line(NULL, (int)(i+((float)i/(1280.0-64))*64.0), (int)(j+((float)j/(1024.0-64))*64.0), (int)((i+64)-((float)i/(1280.0-64))*64.0), (int)((j+64)-((float)j/(1024.0-64))*64.0),	//x,y,x2,y2
 										(float)((i)/1280.0), (float)((i+j)/1302.0), (float)((j)/1024.0), 	// stroke color
 										(float)((double)i/(1280.0-64.0))*4+1.0);			//stroke size										 
-				//TenMilManUI::instance()->addDisplayObject(tmpLine);
 				getTenUI()->addDisplayObject(tmpLine);
 			}
 		}
-	}
+	}*/
 	
-	void TMMUI_TestApp::initRasterTest(){
-		/*Rasterable *tmpRaster = new Rasterable(NULL, 1024,640);
-		tmpRaster->setTextureFromFile("TestResources/RasterTest/bomber_blue.png");
-		sltMgr->regis{
-		{
-			
-		}terObj((UIComponent*)tmpRaster);
-		getTenUI()->addDisplayObject(tmpRaster);*/
-
+	/*void TMMUI_TestApp::initRasterTest(){
 		Rasterable *tmpRaster = new Rasterable(NULL, 1034,640);
 		tmpRaster->setTextureFromFile("TestResources/RasterTest/bomber_red.png");
-		sltMgr->registerObj((UIComponent*)tmpRaster);
-		getTenUI()->addDisplayObject(tmpRaster);
-
 		
-		/*tmpRaster = new Rasterable(NULL, 64,64);
-		tmpRaster->setTextureFromFile("TestResources/RasterTest/barney.jpg");
-		sltMgr->registerObj((UIComponent*)tmpRaster);
 		getTenUI()->addDisplayObject(tmpRaster);
-		
-		tmpRaster = new Rasterable(NULL, 704,64);
-		tmpRaster->setTextureFromFile("TestResources/RasterTest/keepon_motion.jpg");
-		sltMgr->registerObj((UIComponent*)tmpRaster);
-		getTenUI()->addDisplayObject(tmpRaster);*/
-	}
+	}*/
 	
-	void TMMUI_TestApp::initTextTest(){
+	/*void TMMUI_TestApp::initTextTest(){
 		Rectangle *tmpRect = new Rectangle(NULL, 64, 768, 640, 64, 			// x,y,w,h
 											  .1,0,.8, 	// fill color
 											  0,0,0, 	// stroke color
@@ -103,23 +211,28 @@ namespace TenUI {
 		getTenUI()->addDisplayObject((DisplayObject*)text);
 		
 		
-	}
+	}*/
 	
 
 	void TMMUI_TestApp::init() throw(int) {
-		sltMgr = new SelectManager();
-		sltMgr->init();
-
+		
+		Rectangle *bg = new Rectangle(NULL, 0, 0, getScreenWidth(), getScreenHeight(), 1.0, 1.0, 1.0,1.0,1.0,1.0,1.0);
+		getTenUI()->addDisplayObject(bg);
+		
+		initChildrenTest();
+		//initLotsOfRectanglesTest();
 		//initInteractiveRectangleTest();
-		initRectangleTest();
+		//initRectangleTest();
 		//initLineTest();
 		//initTextTest();
 		//initRasterTest();						
 	}
 	
 	void TMMUI_TestApp::update_frame() throw(APPEXCEPTIONS) {
-		sltMgr->update();
 	}
 	
 }
+
+
+
 
--- TMMUI_TestApp/TMMUI_TestApp/TMMUI_TestApp.h
+++ TMMUI_TestApp/TMMUI_TestApp/TMMUI_TestApp.h
@@ -8,15 +8,13 @@
 #include <TenMilManUI/TenMilManUI.h>
 #include <TenMilManUI/TenMilManUI_Enums.h>
 #include <TenMilManUI/UserInputs/SDLMouseInput.h>
-#include <TenMilManUI/UserInputs/TableInput.h>
+//#include <TenMilManUI/UserInputs/TableInput.h>
+#include <TenMilManUI/UserInputs/TableInput_NewArch.h>
 #include <TenMilManUI/Graphics/Vector/Rectangle.h>
 #include <TenMilManUI/Graphics/Vector/Line.h>
 #include <TenMilManUI/Graphics/Text/Text.h>
 #include <TenMilManUI/Graphics/Raster/Rasterable.h>
 #include <TenMilManUI/Graphics/Font/FontManager_FT2.h>
-
-#include <TenMilManUI/UI/Controls/InteractiveRectangle.h>
-#include <TenMilManUI/UI/Managers/SelectManager.h>
 
 using namespace std;
 
@@ -35,6 +33,8 @@ namespace TenUI {
 		/*********** Private Methods ***********/
 		/***************************************/
 		private:
+			void initChildrenTest();
+			void initLotsOfRectanglesTest();
 			void initInteractiveRectangleTest();
 			void initRectangleTest();
 			void initLineTest();		
@@ -57,26 +57,35 @@ namespace TenUI {
 			virtual int getScreenBPP()						{ return 32; }
 			virtual TenUI::SCREEN_OPTION getScreenOptions()	{return TenUI::WINDOWED;}
 			
-			//TODO: enable multiple UserInputs here
-			//virtual UserInput* getUserInput()				{ return TableInput::createInstance(); }
-			//virtual UserInput* getUserInput()				{ return SDLMouseInput::createInstance(); }
-			virtual void initializeUserInput(){//				{ cout << "Tbl:\t" << TableInput::createInstance() << "\n"; cout << "SDL:\t" << SDLMouseInput::createInstance() << "\n"; }
-				UserInput* tbInput = TableInput::createInstance(0);
-				//UserInput* tbInput2 = TableInput::createInstance(1);
-				UserInput* mInput = SDLMouseInput::createInstance();
-				cout << "Tbl:\t" << tbInput << "\n";
-				cout << "\tData format: " << tbInput->getDataFormat() << "\n";
-				cout << "\t    UI type: " << tbInput->getType() << "\n";
-				/*cout << "Tbl:\t" << tbInput2 << "\n";
-				cout << "\tData format: " << tbInput2->getDataFormat() << "\n";
-				cout << "\t    UI type: " << tbInput2->getType() << "\n";*/
-				cout << "SDL:\t" << mInput << "\n";
-				cout << "\tData format: " << mInput->getDataFormat() << "\n";
-				cout << "\t    UI type: " << mInput->getType() << "\n";
+			virtual void initializeUserInput(){
+				
+				/*int numTbls = 0;
+				UserInput* tbInput[numTbls];
+				
+				for(int i = 0; i < numTbls; ++i)
+				tbInput[i] = TableInput::createInstance();*/
+				
+				//UserInput* mInput = SDLMouseInput::createInstance();
+				
+				/*for(int i = 0; i < numTbls; ++i)
+				cout << "Tbl:\t" << tbInput << "\n"
+					 << "\t        UID: " << tbInput[i]->getUID() << "\n";*/					 
+				
+				//cout << "SDL:\t" << mInput << "\n";
+				//cout << "\t        UID: " << mInput->getUID() << "\n";
+				
+				getTenUIInputManager()->registerInput(new SDLMouseInput);
+				//TR getTenUIInputManager()->registerInput(new TableInput_NewArch("shirley.vrac.iastate.edu",50002,50003));
+				//TableInput_NewArch *newInput = new TableInput_NewArch("shirley.vrac.iastate.edu", 50002, 50003);
+				
+				//cout << "TblNA:\t" << newInput << "\n";
+				//cout << "\t        UID: " << newInput->getUID() << "\n";
+				
 			}
+			
+
 			virtual string getFontDirectory()				{return "TestResources/FontTest";}
 			
-			SelectManager* sltMgr;
 	};
 	
 }
